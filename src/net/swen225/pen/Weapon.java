dc6a13adbd82536a3a4a5864f30df17462f14e81
LilData1 <87554843+LilData1@users.noreply.github.com>
2021-07-21 16:29:57 +1200

    Update Weapon.java

--- src/net/swen225/pen/Weapon.java
+++ src/net/swen225/pen/Weapon.java
@@ -1,8 +1,49 @@
 package net.swen225.pen;
 
-public class Weapon {
+import net.swen225.pen.board.Movable;
+import net.swen225.pen.board.Position;
+import net.swen225.pen.board.Space;
+
+public class Weapon implements Movable{
   String weapon;
+
   public Weapon(String weapon){
     this.weapon = weapon;
   }
+
+@Override
+public Space getSpace() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public boolean moveToSpace(Space space) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+@Override
+public Position getPosition() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public boolean moveToPosition(Position pos) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+@Override
+public String getName() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public void setName(String name) {
+	// TODO Auto-generated method stub
+
+}
 }
