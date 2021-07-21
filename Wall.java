9832f87b82834bcea4f4ac70224558480c9090a7
SuperSteezie <66284137+SuperSteezie@users.noreply.github.com>
2021-07-21 14:57:43 +1200

    Add files via upload

---
+++ Wall.java
@@ -0,0 +1,7 @@
+
+public class Wall extends Tile{
+	boolean playerOn;
+	public String toString() {
+		return "#";
+	}
+}
---
+++ WhiteSquare.java
@@ -0,0 +1,7 @@
+
+public class WhiteSquare extends Tile{
+	boolean playerOn;
+	public String toString() {
+		return "_";
+	}
+}
