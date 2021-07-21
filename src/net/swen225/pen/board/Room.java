855d31f02c9123ee795f9cad5845af6bda973509
LilData1 <87554843+LilData1@users.noreply.github.com>
2021-07-21 16:30:30 +1200

    Update Room.java

--- src/net/swen225/pen/board/Room.java
+++ src/net/swen225/pen/board/Room.java
@@ -1,10 +1,23 @@
 package net.swen225.pen.board;
 
-public class Room{
+import net.swen225.pen.Evidence;
+
+public class Room implements Evidence{
   String room;
   public Room(String room){
     this.room = room;
   }
+
+@Override
+public String getName() {
+	// TODO Auto-generated method stub
+	return null;
+}
+@Override
+public void setName(String name) {
+	// TODO Auto-generated method stub
+
+}
 }
 
 
