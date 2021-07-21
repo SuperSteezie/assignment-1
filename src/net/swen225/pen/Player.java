172461b39a498b0ec60ea8a22591fdd99357d9ef
LilData1 <87554843+LilData1@users.noreply.github.com>
2021-07-21 16:20:58 +1200

    Update Player.java

--- src/net/swen225/pen/Player.java
+++ src/net/swen225/pen/Player.java
@@ -3,7 +3,7 @@ package net.swen225.pen;
 public class Player {
   String character;
   String name;
-  public Player(character, name){
+  public Player(String character, String name){
     this.character = character;
     this.name = name;
     
