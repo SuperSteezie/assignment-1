e313a7482792d01d8d6a88302372110bdaf3c6f0
cookidgod84 <65999315+cookidgod84@users.noreply.github.com>
2021-07-21 14:37:49 +1200

    Update Room.java

--- src/net/swen225/pen/board/Room.java
+++ src/net/swen225/pen/board/Room.java
@@ -1,15 +1,7 @@
 package net.swen225.pen.board;
 
-public class Room implements Tile {
-  private GameBoard board;
-  private Room room;
-  private char direction;//direction to leave
-  
-  public Room(GameBoard board, Room room, char direction){
-    this.board = board;
-    this.room = room;
-    this.direction = direction;
-  }
-  
+public class Room{
   
 }
+
+
