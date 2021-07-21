7b42873a34af83cb7a6bb74e5df2a86696a64de5
cookidgod84 <65999315+cookidgod84@users.noreply.github.com>
2021-07-21 16:24:52 +1200

    Update Player.java

--- src/net/swen225/pen/Player.java
+++ src/net/swen225/pen/Player.java
@@ -1,7 +1,7 @@
 package net.swen225.pen;
 
-public class Player {
-  String character;
+public class Player implements Moveable {
+  String character
   String name;
   public Player(String character, String name){
     this.character = character;
