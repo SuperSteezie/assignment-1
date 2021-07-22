8da73b1bee579bf94524f50d7198ac95ec673050
cookidgod84 <65999315+cookidgod84@users.noreply.github.com>
2021-07-22 12:59:57 +1200

    Update Room.java

--- src/net/swen225/pen/board/Room.java
+++ src/net/swen225/pen/board/Room.java
@@ -1,23 +1,46 @@
 package net.swen225.pen.board;
 
-import net.swen225.pen.Evidence;
-
-public class Room implements Evidence{
-  String room;
-  public Room(String room){
-    this.room = room;
+public class Room{
+  private String roomname;
+  private Set<Moveable> moveables = new HashSet<Moveable>();
+  public Room(String roomname){
+    this.roomname = roomname;
   }
 
-@Override
-public String getName() {
-	// TODO Auto-generated method stub
-	return null;
-}
-@Override
-public void setName(String name) {
-	// TODO Auto-generated method stub
 
-}
+  /**
+   * moves a Moveable from this room to another
+   *
+   * @param the room to move the Moveable to
+   * @param the Moveable to be moved
+   */
+  public void shiftMoveable(Room newroom, Moveable moveable){
+    for(Moveable current : moveables){
+      if(current.equals(moveable)){
+        newroom.addMoveable(current);
+        moveables.remove(current);
+        return;
+      }
+    }
+  }
+
+  public void addMoveable(Moveable moveable){
+    moveables.add(moveables);
+  }
+
+
+  /**
+   * returns .charIcon() values of all moveables to be used for drawing
+   *
+   * @return the list containing char representations of every moveable in the room
+   */
+  public List<char> getMoveables(){
+    List<char> returner = new ArrayList<char>();
+    for(Moveable current : moveables){
+      returner.add(current.charIcon());
+    }
+    return returner;
+  }
 }
 
 
